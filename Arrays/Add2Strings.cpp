91db904664bc8bf97275ac21e992a95660cbe2ee
harshpreet931 <hsanime@gmail.com>
2024-01-18 09:31:25 +0530

    Adding 2 Strings

--- Arrays/Add2Strings.cpp
+++ Arrays/Add2Strings.cpp
@@ -1,34 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
- // do this and 3rd question on leetcode
- // currently wrong
+
 int main() {
     string s1 = "1294";
-    string s2 = "5432100";
+    string s2 = "540000";
 
     string res = "";
 
     int carry = 0;
 
-    for(int i = s2.size() - 1; i >= 0; i--) {
-        int curr = (s2[i] - '0') + (s1[i] - '0') + carry;
+    int i = s1.size()-1;
+    int j = s2.size()-1;
 
-        if(curr > 9) {
-            carry = 1;
-            curr %= 10;
-        } else {
-            carry = 0;
-        }
-
-        res[i] = (char)(curr + '0');
+    while(i >= 0 || j >= 0){
+        int d1 = i >= 0 ? s1[i]-'0' : 0;
+        int d2 = j >= 0 ? s2[j]-'0' : 0;
+        int sum = d1 + d2 + carry;
+        res += (char)(sum%10 + '0');
+        carry = sum / 10;
+        i--; j--;
     }
-
-    if (carry > 0) {
-        res.insert(s2.begin(), '1');
-    }
-
+    if(carry == 1) res += '1';
+    reverse(res.begin(),res.end());
     cout << res << endl;
-
-
     return 0;
 }
